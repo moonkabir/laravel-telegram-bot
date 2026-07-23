e2fee69770581c1ca5d39fa2a8f1eb3d39d8777d
moonkabir <moonkabir4@gmail.com>
2026-07-23 15:18:25 +0600

    Refactor document routes for improved organization and clarity

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\DocumentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Document routes
+Route::get('/documents', [DocumentController::class, 'index'])->name('documents.index');
+Route::post('/documents/upload', [DocumentController::class, 'upload'])->name('documents.upload');
+Route::delete('/documents/{id}', [DocumentController::class, 'delete'])->name('documents.delete');
+Route::get('/documents/search', [DocumentController::class, 'search'])->name('documents.search');
+Route::get('/documents/{id}', [DocumentController::class, 'show'])->name('documents.show');
